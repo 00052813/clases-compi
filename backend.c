f333109d5baff60545bad26282ea1bcdf794c6bc
00052813 <35278729+00052813@users.noreply.github.com>
2018-01-09 21:14:52 -0600

    Update backend.c

--- backend.c
+++ backend.c
@@ -23,8 +23,30 @@ static void Gen_codigo_expresion(Expresion *expr) {
   }
 }
 
+static void Int_codigo(Expresion *expr){
+switch(expr->tipo) {
+    case 'D':
+      //printf("PUSH %d\n",expr->valor);
+      return expr->valor
+      break;
+    case 'P':
+      //Gen_codigo_expresion(expr->izquierda);
+      //Gen_codigo_expresion(expr->derecha);
+      switch (expr->oper) {
+        case '+':
+          //printf("ADD\n");
+          return Gen_codigo_expresion(expr->izquierda) + Gen_codigo_expresion(expr->derecha);
+          break;
+        case '*':
+          return Gen_codigo_expresion(expr->izquierda) * Gen_codigo_expresion(expr->derecha);
+          break;
+      }
+      break;
+  }
+}
+
 
 void Procesar(nodo_AST *codigoi){
-  Gen_codigo_expresion(codigoi);
-  printf("PRINT\n");
+  //Gen_codigo_expresion(codigoi);
+  printf("%d",Int_codigo(codigoi));
 }
